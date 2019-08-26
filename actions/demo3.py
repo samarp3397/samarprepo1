df80e8b150a363cd2d34926a9f94d4bac326cfa7
samarp3397 <54352997+samarp3397@users.noreply.github.com>
2019-08-26 14:30:36 +0530

    Update demo3.py

--- actions/demo3.py
+++ actions/demo3.py
@@ -6,12 +6,12 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
         def run(self,id,title,desc,pagecount,excerpt,pubdate):
-           try:
-              	x={"ID": id,"Title": title,"Description": desc,"PageCount": pagecount,"Excerpt": excerpt,"PublishDate": pubdate}
-                y=json.dumps(x)
-		url=https://fakerestapi.azurewebsites.net/api/Books
-		res=requests.post(url,data=x)
-		print(y)
+                try:
+              		x={"ID": id,"Title": title,"Description": desc,"PageCount": pagecount,"Excerpt": excerpt,"PublishDate": pubdate}
+                	y=json.dumps(x)
+			url=https://fakerestapi.azurewebsites.net/api/Books
+			res=requests.post(url,data=x)
+			print(y)
 
-	     except:
-                    sys.exit(0)
+	        except:
+                    	sys.exit(0)
