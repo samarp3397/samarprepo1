f5b8dcfc86febfadaba400721e5e917df518319a
samarp3397 <54352997+samarp3397@users.noreply.github.com>
2019-08-27 10:05:29 +0530

    Update demo4.py

--- actions/demo4.py
+++ actions/demo4.py
@@ -7,11 +7,13 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
      def run(self,id):
         try:
-                x={"ID": id}
-                url='https://fakerestapi.azurewebsites.net/api/Books/9'
-                res=requests.get(url)
+                id1= str(id);
+                headers={'content-type': 'application/json'}
+                url='https://fakerestapi.azurewebsites.net/api/Books/'+id1
+                res=requests.get(url,headers=headers,timeout=6.0)
                 print(res)
                 y=res.json()
                 print(y)
-        except:
-                sys.exit(0)
+        except requets.exceptions.Timeout:
+               print("Request timeout")
+               sys.exit(0)
